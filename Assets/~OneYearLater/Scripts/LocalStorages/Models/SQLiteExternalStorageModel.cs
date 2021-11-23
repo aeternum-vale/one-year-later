0b37a663da63da3cf11fd5984c62b8225a58526b
Artur Dedyulya <aeternum-vale@ya.ru>
2021-11-23 19:06:14 +0300

    change names of tables and fields in sqlite db according to app models

--- Assets/~OneYearLater/Scripts/LocalStorages/Models/SQLiteExternalStorageModel.cs
+++ Assets/~OneYearLater/Scripts/LocalStorages/Models/SQLiteExternalStorageModel.cs
@@ -3,17 +3,11 @@ using System;
 
 namespace OneYearLater.LocalStorages.Models
 {
-	[Table("external_storage")]
 	class SQLiteExternalStorageModel
 	{
 		[PrimaryKey]
-		[Column("id")]
 		public int Id { get; set; }
-
-		[Column("state")]
 		public string State { get; set; }
-
-		[Column("date_time")]
 		public DateTime? LastSync { get; set; } = null;
 	}
 }
