8fb1fbaa79ca0d41f80ca0574f34ecb07c1839e8
Artur Dedyulya <aeternum-vale@ya.ru>
2021-12-17 11:13:07 +0300

    add moq and assembly definitions for tests

---
+++ Assets/~OneYearLater/Scripts/Tests/EditMode/SynchronizerTests.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using NUnit.Framework;
+using UnityEngine;
+using UnityEngine.TestTools;
+
+namespace OneYearLater.Tests
+{
+	public class SynchronizerTests
+	{
+		[Test]
+		public void TestTwoEqualsTwo()
+		{
+			Assert.AreEqual(2, 2);
+		}
+
+	}
+}
